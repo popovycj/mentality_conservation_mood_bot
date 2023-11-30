d59ffac2bf27eb8bad431af9f2b90b10f3683ef5
Marian <marian.popovych.ua@gmail.com>
2023-12-01 01:12:22 +0200

    Move on questions and answers to i18n locale file

--- app/services/telegram_handlers/preferred_language_handler.rb
+++ app/services/telegram_handlers/preferred_language_handler.rb
@@ -1,19 +1,12 @@
 module TelegramHandlers
   class PreferredLanguageHandler < BaseHandler
-    QUESTIONS = [
-      "What languages do you prefer?"
-    ].freeze
-
-    ANSWER_SCORES = {
-      'All' => [:ukrainian, :russian, :english],
-      'Not russian' => [:ukrainian, :english],
-      'Only Ukrainian' => [:ukrainian]
-    }.freeze
+    QUESTIONS = I18n.t("telegram_handlers.#{self.name.demodulize.underscore}.questions").freeze
+    ANSWER_SCORES = I18n.t("telegram_handlers.#{self.name.demodulize.underscore}.answer_scores").freeze
 
     STATE_EXPIRATION = 24.hours
 
     def initialize(bot, user_id)
-      super(bot, user_id, QUESTIONS, ANSWER_SCORES, STATE_EXPIRATION)
+      super(bot, user_id, QUESTIONS, ANSWER_SCORES.transform_keys(&:to_s), STATE_EXPIRATION)
     end
 
     def process_answers(answers)
--- app/services/telegram_handlers/preferred_release_time_range_handler.rb
+++ app/services/telegram_handlers/preferred_release_time_range_handler.rb
@@ -1,19 +1,12 @@
 module TelegramHandlers
   class PreferredReleaseTimeRangeHandler < BaseHandler
-    QUESTIONS = [
-      "What release time range do you prefer?"
-    ].freeze
-
-    ANSWER_SCORES = {
-      'To 2010' => (1900..2010),
-      'From 2010 to present' => (2010..Time.now.year),
-      'From the beginning of the war' => (2022..Time.now.year)
-    }.freeze
+    QUESTIONS = I18n.t("telegram_handlers.#{self.name.demodulize.underscore}.questions").freeze
+    ANSWER_SCORES = I18n.t("telegram_handlers.#{self.name.demodulize.underscore}.answer_scores").freeze
 
     STATE_EXPIRATION = 24.hours
 
     def initialize(bot, user_id)
-      super(bot, user_id, QUESTIONS, ANSWER_SCORES, STATE_EXPIRATION)
+      super(bot, user_id, QUESTIONS, ANSWER_SCORES.transform_keys(&:to_s), STATE_EXPIRATION)
     end
 
     def process_answers(answers)
